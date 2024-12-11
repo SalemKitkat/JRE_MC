053288eafb9e5e8da1e8c68d08560311273967c7
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2024-12-11 16:34:39 +0000

    Update 1_16.bat

--- SubModule/1_16.bat
+++ SubModule/1_16.bat
@@ -5,7 +5,7 @@ If "%1"=="" Goto Relay
 :Start
 If Exist Peridot-Relay.txt (
 	Set /P serverJar=<Peridot-Relay.txt
-	"C:\Program Files\Java\jre1.8.0_431\bin\java.exe" -Xms6G -Xmx6G -jar %serverJar% nogui
+	"C:\Program Files\Java\jre1.8.0_431\bin\java.exe" -Xms10G -Xmx10G -jar %serverJar% nogui
 ) Else (
 	"C:\Program Files\Java\jre1.8.0_431\bin\java.exe" -Xms10G -Xmx10G -jar %1 nogui
 )
