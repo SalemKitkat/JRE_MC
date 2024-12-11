60d25481fa5fda9ae3e481e2ec4829ec156f4d3d
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2024-12-11 16:31:55 +0000

    SubModule: 1.16.x - Fix old relics

--- SubModule/1_16.bat
+++ SubModule/1_16.bat
@@ -1,9 +1,4 @@
 @Echo off
-:Start
-"C:\Program Files\Java\jre1.8.0_431\bin\java.exe" -Xms10G -Xmx10G -jar %1 nogui
-Goto Start
-
-@Echo off
 SetLocal EnableExtensions EnableDelayedExpansion
 If "%1"=="" Goto Relay
 
